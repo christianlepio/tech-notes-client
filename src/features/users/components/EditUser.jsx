d9e827012244e42c23d61762291d5fbc7ba7d23e
christianlepio <rylepio79@gmail.com>
2024-01-07 01:38:45 +0800

    pull id from url using useParams & setup EditUser component

--- src/features/users/components/EditUser.jsx
+++ src/features/users/components/EditUser.jsx
@@ -1,9 +1,22 @@
-
+// get userid value parameter from url
+import { useParams } from "react-router-dom"
+// useSelector: to get global state variable from store
+import { useSelector } from "react-redux"
+// memoized selector 
+import { selectUserById } from "../usersApiSlice"
+// component
+import EditUserForm from "./EditUserForm"
 
 const EditUser = () => {
-    return (
-        <div>EditUser</div>
-    )
+    // pull userid from url using useParams()
+    const { id } = useParams()
+
+    // get specific user details
+    const user = useSelector(state => selectUserById(state, id))
+
+    const content = user ? <EditUserForm user={user} /> : <p>Loading . . .</p>
+
+    return content
 }
 
 export default EditUser
