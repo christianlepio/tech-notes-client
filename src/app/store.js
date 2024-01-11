98e062ab018c856a5d71ab905da322fd775c654f
christianlepio <rylepio79@gmail.com>
2024-01-11 14:33:42 +0800

    attach auth reducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit"
 // this contains RTK query (endpoints) for managing notes and users
 import { apiSlice } from "./api/apiSlice"
 import { setupListeners } from "@reduxjs/toolkit/query"
+import authReducer from '../features/auth/authSlice'
 
 // config
 export const store = configureStore({
@@ -9,6 +10,7 @@ export const store = configureStore({
     // components can call/use all reducers here from store
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer, // default property name of this is 'api'
+        auth: authReducer // reducer for login auth
     },
     // this is required when implementing RTK query with store
     middleware: getDefaultMiddleware => 
