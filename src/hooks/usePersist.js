f3b8816a6f5ea83942f5430763e54ef5523543e6
christianlepio <rylepio79@gmail.com>
2024-01-12 15:47:15 +0800

    create usePersist custom hook

--- src/hooks/usePersist.js
+++ src/hooks/usePersist.js
@@ -1,9 +1,12 @@
+// custom hook to persist login and not go back to 1st page once logged in when refresh page
 import { useState, useEffect } from 'react'
 
 const usePersist = () => {
+    // get persist variable from local storage persist, if not exist then set persist to false
     const [persist, setPersist] = useState(JSON.parse(localStorage.getItem('persist')) || false )
 
     useEffect(() => {
+        // set persist local storage value when persist variable changes
         localStorage.setItem('persist', JSON.stringify(persist))
     }, [persist])
 
