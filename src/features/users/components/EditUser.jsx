f8fa6213386c84429208258351dfe8e4ab12ecf2
christianlepio <rylepio79@gmail.com>
2024-01-17 01:01:33 +0800

    get specific user by id by calling query endpoint getUsers

--- src/features/users/components/EditUser.jsx
+++ src/features/users/components/EditUser.jsx
@@ -1,9 +1,10 @@
 // get userid value parameter from url
 import { useParams } from "react-router-dom"
 // useSelector: to get global state variable from store
-import { useSelector } from "react-redux"
+// import { useSelector } from "react-redux"
 // memoized selector 
-import { selectUserById } from "../usersApiSlice"
+// import { selectUserById } from "../usersApiSlice"
+import { useGetUsersQuery } from "../usersApiSlice"
 // component
 import EditUserForm from "./EditUserForm"
 
@@ -12,7 +13,15 @@ const EditUser = () => {
     const { id } = useParams()
 
     // get specific user details
-    const user = useSelector(state => selectUserById(state, id))
+    // const user = useSelector(state => selectUserById(state, id))
+
+    // get specific user by id by calling query endpoint getUsers
+    const { user } = useGetUsersQuery('usersList', {
+        // built in function to get/return specific user
+        selectFromResult: ({ data }) => ({
+            user: data?.entities[id]
+        }),
+    })
 
     const content = user ? <EditUserForm user={user} /> : <p>Loading . . .</p>
 
