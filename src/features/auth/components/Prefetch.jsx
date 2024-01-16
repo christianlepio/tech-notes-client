b507bdf2d002dcfafd5407c6b136320f42e9edcb
christianlepio <rylepio79@gmail.com>
2024-01-16 21:11:12 +0800

    modify code for prefetch

--- src/features/auth/components/Prefetch.jsx
+++ src/features/auth/components/Prefetch.jsx
@@ -2,6 +2,8 @@
 // it suddenly expires the cached data then instead of going to edit form page it will only 
 // display 'loading ...'. Also, when you refresh the page it will still remain on edit form page.
 
+// prefetch is also initiating state from redux
+
 import { store } from '../../../app/store'
 import { notesApiSlice } from '../../notes/notesApiSlice'
 import { usersApiSlice } from '../../users/usersApiSlice'
@@ -9,16 +11,22 @@ import { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 
 const Prefetch = () => {
+    
     useEffect(() => {
-        console.log('Prefetch subscribing!...')
-        const notes = store.dispatch(notesApiSlice.endpoints.getNotes.initiate())
-        const users = store.dispatch(usersApiSlice.endpoints.getUsers.initiate())
+        // console.log('Prefetch subscribing!...')
+        // const notes = store.dispatch(notesApiSlice.endpoints.getNotes.initiate())
+        // const users = store.dispatch(usersApiSlice.endpoints.getUsers.initiate())
 
-        return () => {
-            console.log('Prefetch unsubscribing!...')
-            notes.unsubscribe()
-            users.unsubscribe()
-        }
+        // prefetch method, this needs the endpoint name, then the argument name that holds the data
+        // force obj true is to force rtk query to prefetch again every time the compoments render
+        store.dispatch(notesApiSlice.util.prefetch('getNotes', 'notesList', { force: true }))
+        store.dispatch(usersApiSlice.util.prefetch('getUsers', 'usersList', { force: true }))
+
+        // return () => {
+        //     console.log('Prefetch unsubscribing!...')
+        //     notes.unsubscribe()
+        //     users.unsubscribe()
+        // }
     }, [])
 
     // all of the protected routes will be wrapped here
