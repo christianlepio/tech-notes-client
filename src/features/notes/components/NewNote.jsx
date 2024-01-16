a1371d71daf8e95f10c17bfee7e64bb4909ada17
christianlepio <rylepio79@gmail.com>
2024-01-17 01:07:12 +0800

    get all users by mapping ids and getting data from entities by calling query endpoint getUsers

--- src/features/notes/components/NewNote.jsx
+++ src/features/notes/components/NewNote.jsx
@@ -1,12 +1,21 @@
-import { useSelector } from "react-redux"
+// import { useSelector } from "react-redux"
 // memoized users data 
-import { selectAllUsers } from "../../users/usersApiSlice"
+// import { selectAllUsers } from "../../users/usersApiSlice"
+import { useGetUsersQuery } from "../../users/usersApiSlice"
 // component 
 import NewNoteForm from "./NewNoteForm"
 
 const NewNote = () => {
     // get users data from selectors
-    const users = useSelector(selectAllUsers)
+    // const users = useSelector(selectAllUsers)
+
+    // get all users by mapping ids and getting data from entities by calling query endpoint getUsers
+    const { users } = useGetUsersQuery('usersList', {
+        // built in function to get/return all users
+        selectFromResult: ({ data }) => ({
+            user: data?.ids.map(id => data?.entities[id])
+        }),
+    })
 
     const content = users ? <NewNoteForm users={users} /> : <p>Loading . . .</p>
     
